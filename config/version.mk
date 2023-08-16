0825c5119ca4dff09b94bf6361e505d93e858dff
Christopher Keith Irvine <Mizdrake7@gmail.com>
2023-08-16 20:10:11 +0530

    vendor: `2.7.3-Electrified`

--- config/version.mk
+++ config/version.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 ANDROID_VERSION := 13
-VOLTAGEVERSION := 2.7.2-Electrified
+VOLTAGEVERSION := 2.7.3-Electrified
 
 VOLTAGE_BUILD_TYPE ?= UNOFFICIAL
 VOLTAGE_DATE_YEAR := $(shell date -u +%Y)
